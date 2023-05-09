88b61f10fd2a012a0f603a3de31f3cf1c94815a6
Štěpán Chlumský <chlumskystepan@seznam.cz>
2023-05-09 12:54:22 +0200

    Evidence prodejcu mrkve

--- src/EvidenceProdejcu.java
+++ src/EvidenceProdejcu.java
@@ -1,19 +1,35 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.time.LocalDate;
 public class EvidenceProdejcu {
 
-    public class Prodejce {
-        static String jmeno = "Karel";
-        static String prijmeni = "Novotný";
-        LocalDate datumNarozeni = LocalDate.of (1995,5 ,3);
-        int sjednaneSmlouvy = 3;
-        int vahaMrkve = 2;
-        String bydliste = "Liberec";
+     static class Prodejce {
+        static String jmeno = "Karel"; //Jméno
+        static String prijmeni = "Novotný"; //Příjmení
+        static LocalDate datumNarozeni = LocalDate.of (1995,5 ,3); //Datum nazorení
+        static int sjednaneSmlouvy = 1; //Počet sjednaných smluv
+        static double prodaneMrkve = 5.3; //Celkové množství prodané mrkve v tunách
+        static String bydliste = "Liberec"; //Název města, kde prodejce sídlí
+        static String SPZ = "6L2 1546"; //Registrační značka vozidla
+        static double spotrebaVozidla = 7.6; //Spotřeba firemního vozidla
+        static byte[] addr = { 127, 0, 0, 1 }; //IP adresa verze 4 firemního počítače
+         static InetAddress adresa_IPv4;
 
+         static {
+             try {
+                 adresa_IPv4 = InetAddress.getByAddress(addr);
+             } catch (UnknownHostException e) {
+                 throw new RuntimeException(e);
+             }
+         }
 
-    }
+     }
 
     public static void main(String[] args) {
         EvidenceProdejcu myObj = new EvidenceProdejcu();
-        System.out.println(Prodejce.jmeno  Prodejce.prijmeni);
+        float prumer = (float)((Prodejce.sjednaneSmlouvy + Prodejce.prodaneMrkve) / 2);
+        System.out.println("Průměr prodaného množství mrkve na jednu smlouvu: "+prumer);
+
+
     }
 }
